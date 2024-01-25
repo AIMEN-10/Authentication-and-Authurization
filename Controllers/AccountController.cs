f712243ab37a0e29f42c464523dd263cc6adac37
AIMEN-10 <aimenkhalid003@gmail.com>
2024-01-25 23:26:24 +0500

    Updated

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Authentication_and_Authurization.Models;
+using System.Web.Security;
+
+namespace Authentication_and_Authurization.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Models.Membership model)
+        {
+            using (var context = new OfficeEntities())
+            {
+                bool isValid = context.Users.Any(x => x.UserName == model.Name && x.Password == model.Password);
+                if (isValid)
+                {
+                    FormsAuthentication.SetAuthCookie(model.Name, false);
+                    return RedirectToAction("Index", "Employees");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Invalid User");
+
+                }
+            }
+            return View();
+        }
+        public ActionResult Signup()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Signup(User model)
+        {
+            using (var context = new OfficeEntities())
+            {
+                context.Users.Add(model);
+                context.SaveChanges();
+            }
+            return RedirectToAction("Login");
+        }
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
